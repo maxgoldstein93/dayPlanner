a0d8bb5e0db517dc4359eb0d6a8990ab1cd2c117
Max Goldstein <magoldstein93@gmail.com>
2020-09-11 15:20:34 -0400

    Working Local storage for all times

--- dailyplanner.js
+++ dailyplanner.js
@@ -5,21 +5,59 @@ console.log(todaysDate);
 date.text(todaysDate)
 
 // Save user input
-$(".saveBtn").on("click", function () {
+$(".saveBtn").on("click", function (event) {
+    event.preventDefault()
     console.log("Save");
-    var inputTask = document.querySelector("#nineAm").value.trim();
+    var inputTask = $(this).prev().find("input").val().trim();
     console.log(inputTask)
-    localStorage.setItem("nineAm",JSON.stringify(inputTask));
-    
+    localStorage.setItem($(this).prev().find("input").attr("id"),(inputTask));
+
+
     
 });
 
+            // Get from Storage 
+    var displayTask =localStorage.getItem("nineAm");
+    var displayUserTask =document.querySelector("#nineAm");
+    displayUserTask.value = displayTask;
+
+    var displayTask =localStorage.getItem("tenAm");
+    var displayUserTask =document.querySelector("#tenAm");
+    displayUserTask.value = displayTask;
+
+    var displayTask =localStorage.getItem("elevenAm");
+    var displayUserTask =document.querySelector("#elevenAm");
+    displayUserTask.value = displayTask;
+
+    var displayTask =localStorage.getItem("twelvePm");
+    var displayUserTask =document.querySelector("#twelvePm");
+    displayUserTask.value = displayTask;
+
+    var displayTask =localStorage.getItem("onePm");
+    var displayUserTask =document.querySelector("#onePm");
+    displayUserTask.value = displayTask;
+
+    var displayTask =localStorage.getItem("twoPm");
+    var displayUserTask =document.querySelector("#twoPm");
+    displayUserTask.value = displayTask;
+
+    var displayTask =localStorage.getItem("threePm");
+    var displayUserTask =document.querySelector("#threePm");
+    displayUserTask.value = displayTask;
+
+    var displayTask =localStorage.getItem("fourPm");
+    var displayUserTask =document.querySelector("#fourPm");
+    displayUserTask.value = displayTask;
+
+    var displayTask =localStorage.getItem("fivePm");
+    var displayUserTask =document.querySelector("#fivePm");
+    displayUserTask.value = displayTask;
+    
+    
 
 
-// var displayTask =localStorage.getItem("nineAm");
-// var displayTask =localStorage.getItem(inputTask);
-// var displayUserTask =document.querySelector("#nineAm");
-//     displayUserTask.textContent = displayTask;
+
+    
 
 // //  Advanced Rows and Columns
 // var workHours = ["1","2","3","4","5","6","7","8"]
@@ -30,7 +68,7 @@ $(".saveBtn").on("click", function () {
 
 // };
 // Hour variable for document
-// var hour =moment().format("H");
+// var hour =moment().format("HH");
 // console.log(hour)
 
 
@@ -38,11 +76,11 @@ $(".saveBtn").on("click", function () {
 // function () color change
 
 // if (currentTime > H){
-    // display class past
+    // add to input class past
 
 // } else if( current time = H){
-//   display class present
+//   add class present
 // }else{
-// display class future
+// add class future
 // }
 
